9477dc10274757d53104b17807febd467dd95671
Taku Fukata <fukatataku@gmail.com>
2015-03-19 00:38:12 +0900

    update

--- lib/common/common.py
+++ lib/common/common.py
@@ -9,6 +9,7 @@ __HOME__ = abspath(join(__HERE__, '..', '..'))
 RES_DIR = join(__HOME__, 'res')
 CONF_DIR = join(RES_DIR, 'conf')
 TMPL_DIR = join(RES_DIR, 'template')
+RPRT_DIR = join(TMPL_DIR, "report")
 
 MAIN_CONF_FILE = join(CONF_DIR, 'main.conf')
 FILT_CONF_FILE = join(CONF_DIR, 'filter.conf')
