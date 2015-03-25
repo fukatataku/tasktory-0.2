efe8d9b0d1c84c1e70492bac9e54c27b54cef293
Taku Fukata <fukatataku@gmail.com>
2015-03-26 00:10:52 +0900

    update

--- lib/ui/icon/WinTasktoryIcon.py
+++ lib/ui/icon/WinTasktoryIcon.py
@@ -1,13 +1,27 @@
 #!C:/python/python3.4/python
 # -*- coding: utf-8 -*-
 
+import configparser
 from lib.ui.icon.WinTrayIcon import TrayIcon
+from lib.ui.journal.Journal import Journal
+from lib.common.common import MAIN_CONF_FILE
+from lib.common.common import FILT_CONF_FILE
 from lib.common.common import ICON_IMG_FILE
 
 
 class TasktoryIcon(TrayIcon):
 
     def __init__(self):
+        # コンフィグ
+        config = configparser.ConfigParser()
+        config.read(MAIN_CONF_FILE)
+        filt_config = configparser.ConfigParser()
+        filt_config.read(FILT_CONF_FILE)
+
+        # ジャーナル
+        self.journal = Journal(config, filt_config)
+
+        # ポップアップメニュー
         menu = [
                 ('Sync', 0),
                 ('Report', [
