f6bf548c97d46e513d108c23848b9508c4f3c1c6
Taku Fukata <fukatataku@gmail.com>
2015-04-03 14:32:08 +0900

    update

---
+++ lib/ui/journal/JournalObj.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
+class Journal:
+
+    def __init__(self):
+        return
+
+    # TextFile -> JournalObject
+    def read(self):
+        return
+
+    # FileSystem -> JournalObject
+    def checkout(self):
+        return
+
+    # JournalObject -> FileSystem
+    def commit(self):
+        return
+
+    # JournalObject -> TextFile
+    def write(self):
+        return
