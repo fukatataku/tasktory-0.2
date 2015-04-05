8392a1c221d6c850e8379a2f50a5e43b4d3ddc06
Taku Fukata <fukatataku@gmail.com>
2015-04-06 00:41:18 +0900

    update

--- lib/ui/journal/JournalObj.py
+++ lib/ui/journal/JournalObj.py
@@ -1,23 +1,64 @@
 # -*- coding: utf-8 -*-
 
+import os
+from lib.core.Tasktory import Tasktory
+from lib.ui.journal.builder.JournalBuilder import JournalBuilder
+from lib.ui.journal.parser.JournalParser import JournalParser
+from lib.utility.filter.TasktoryFilter import TasktoryFilter
+from lib.common.common import unique
+from lib.common.common import JRNL_TMPL_FILE
 
-class Journal:
 
-    def __init__(self):
+class JournalManager:
+
+    def __init__(self, config, filt_config):
+        with open(JRNL_TMPL_FILE) as f:
+            tmpl = f.read()
+        self.jb = JournalBuilder(tmpl, config)
+        self.jp = JournalParser(tmpl, config)
+        self.jf = TasktoryFilter.get_filter(filt_config['JournalFilter'])
+        self.root = config['Main']['ROOT']
+        self.journal = config['Main']['JOURNAL']
+        return
+
+    # FileSystem -> JournalObject
+    def load(self, date):
+        # ファイルシステムからタスクを復元する
+        root_task = Tasktory.restore(self.root)
+        if root_task is None:
+            tasks = []
+        else:
+            tasks = self.jf.select(root_task)
+
+        # tasksのuniqueを取る
+        tasks = unique(tasks, lambda t: t.path)
+
+        # ジャーナルオブジェクトを作成する
+        return
+
+    # JournalObject -> FileSystem
+    def save(self):
         return
 
     # TextFile -> JournalObject
     def read(self):
-        return
+        # ジャーナルが存在しなければNoneを返す
+        if not os.path.isfile(self.journal):
+            return None
 
-    # FileSystem -> JournalObject
-    def checkout(self):
-        return
+        # ジャーナルテキストを読み出す
+        with open(self.journal, "r", encoding="utf-8-sig") as f:
+            text = f.read()
 
-    # JournalObject -> FileSystem
-    def commit(self):
-        return
+        # ジャーナルを解析する
+        date, attrs_list, memo_list = self.jp.parse(text)
 
     # JournalObject -> TextFile
     def write(self):
+        return
+
+
+class Journal:
+
+    def __init__(self):
         return
